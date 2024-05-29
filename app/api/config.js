cbcf5b9bf45a1846d7a49f587fa1aeed2aa2876f
Leroyalle <nikolajmelonov@yandex.ru>
2024-05-29 15:23:03 +0300

    fix url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://admin-pindie.nomorepartiesco.ru//api';
+export const BASE_URL = 'https://admin-pindie.nomorepartiesco.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
